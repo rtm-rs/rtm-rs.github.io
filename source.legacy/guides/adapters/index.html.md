c86da82632f6f8bc5f4ee2255da0d9078ba78f37
Begley Brothers Ltd <begleybrothers@gmail.com>
2022-05-01 21:05:01 +1000

    Evolve: Remove upstream files
    Signed-off-by: Begley Brothers Ltd <begleybrothers@gmail.com>

--- source.legacy/guides/adapters/index.html.md
+++
@@ -1,7 +0,0 @@
----
-chapter: Adapters
-sections:
-  - how-to-build-an-adapter
----
-
-In this section you can learn how to work with adapters.
--- source.legacy/javascripts/all.js
+++
@@ -1,7 +0,0 @@
-import activateNavigationToggler from './_navigationToggler.js';
-import versiionSwitcher from './_versionSwticher.js';
-
-document.addEventListener('DOMContentLoaded', function() {
-  activateNavigationToggler();
-  versiionSwitcher();
-});
